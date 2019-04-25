fb11bafd88313eaa3e6c7eb16d499fe1c215cd2f
Corey Farrell <git@cfware.com>
2019-04-25 08:38:36 -0400

    feat: Add `Symbol.for('nycrc')`
    Also simplify all code.

--- index.js
+++ index.js
@@ -2,42 +2,35 @@
 
 const {defaultExclude} = require('test-exclude');
 
-class NYCConfig {
-	constructor(settings = {}) {
-		this.settings = {
+const settings = Symbol.for('nycrc');
+
+/* This class exists for members which do not get static copies */
+class NYCConfigBase {
+	constructor(customSettings = {}) {
+		this[settings] = {
 			all: true,
 			require: [],
 			include: [],
 			exclude: [...defaultExclude],
 			excludeNodeModules: true,
-			...settings
+			...customSettings
 		};
 	}
 
-	static fullCoverage(perFile) {
-		return new NYCConfig().fullCoverage(perFile);
+	get settings() {
+		return this[settings];
 	}
+}
 
-	static require(...modules) {
-		return new NYCConfig().require(...modules);
-	}
-
-	static include(...globs) {
-		return new NYCConfig().include(...globs);
-	}
-
-	static exclude(...globs) {
-		return new NYCConfig().exclude(...globs);
-	}
-
+class NYCConfig extends NYCConfigBase {
 	all(enable = false) {
-		this.settings.all = enable;
+		this[settings].all = enable;
 
 		return this;
 	}
 
 	fullCoverage(perFile = true) {
-		Object.assign(this.settings, {
+		Object.assign(this[settings], {
 			checkCoverage: true,
 			perFile,
 			lines: 100,
@@ -49,29 +42,36 @@ class NYCConfig {
 		return this;
 	}
 
-	excludeNodeModules(exclude = false) {
-		this.settings.excludeNodeModules = exclude;
-
-		return this;
-	}
-
 	require(...modules) {
-		this.settings.require.push(...modules);
+		this[settings].require.push(...modules);
 
 		return this;
 	}
 
 	include(...globs) {
-		this.settings.include.push(...globs);
+		this[settings].include.push(...globs);
 
 		return this;
 	}
 
 	exclude(...globs) {
-		this.settings.exclude.push(...globs);
+		this[settings].exclude.push(...globs);
+
+		return this;
+	}
+
+	excludeNodeModules(exclude = false) {
+		this[settings].excludeNodeModules = exclude;
 
 		return this;
 	}
 }
+
+/* Generate static factory methods */
+Object.getOwnPropertyNames(NYCConfig.prototype)
+	.filter(n => n !== 'constructor')
+	.forEach(method => {
+		NYCConfig[method] = (...args) => new NYCConfig()[method](...args);
+	});
 
 module.exports = NYCConfig;
--- test/test.js
+++ test/test.js
@@ -3,129 +3,66 @@ import {defaultExclude} from 'test-exclude';
 
 import NYCConfig from '..';
 
-test('export', t => {
+const snapshotCleanup = ({settings}) => ({
+	...settings,
+	exclude: settings.exclude.filter(e => !defaultExclude.includes(e))
+});
+
+function booleanArgTest(t) {
+	const method = t.title;
+
+	t.true(NYCConfig[method]() instanceof NYCConfig);
+
+	t.deepEqual(NYCConfig[method]().settings.exclude, defaultExclude);
+
+	t.deepEqual(NYCConfig[method](), new NYCConfig()[method]());
+	t.snapshot(snapshotCleanup(NYCConfig[method]()), 'no arg');
+
+	t.deepEqual(NYCConfig[method](true), new NYCConfig()[method](true));
+	t.snapshot(snapshotCleanup(NYCConfig[method](true)), 'true');
+
+	t.deepEqual(NYCConfig[method](false), new NYCConfig()[method](false));
+	t.snapshot(snapshotCleanup(NYCConfig[method](false)), 'false');
+}
+
+function stringsArgTest(t, snaps) {
+	const method = t.title;
+
+	t.true(NYCConfig[method]() instanceof NYCConfig);
+
+	t.deepEqual(NYCConfig[method]().settings.exclude, defaultExclude);
+
+	t.deepEqual(NYCConfig[method](), new NYCConfig().exclude());
+	t.snapshot(snapshotCleanup(NYCConfig[method]()), 'no arg');
+
+	Object.entries(snaps).forEach(([snapName, args]) => {
+		t.snapshot(snapshotCleanup(NYCConfig[method](...args)), snapName);
+
+		t.deepEqual(NYCConfig[method](...args), new NYCConfig()[method](...args));
+	});
+}
+
+test('basics', t => {
 	t.is(typeof NYCConfig, 'function');
-});
 
-test('fullCoverage', t => {
-	t.is(typeof NYCConfig.fullCoverage, 'function');
-	t.true(NYCConfig.fullCoverage() instanceof NYCConfig);
-
-	t.deepEqual(NYCConfig.fullCoverage().settings.exclude, defaultExclude);
-
-	t.snapshot({
-		...NYCConfig.fullCoverage().settings,
-		exclude: []
-	}, 'no arg');
-
-	t.snapshot({
-		...NYCConfig.fullCoverage(true).settings,
-		exclude: []
-	}, 'true');
-
-	t.snapshot({
-		...NYCConfig.fullCoverage(false).settings,
-		exclude: []
-	}, 'false');
-
-	t.deepEqual(NYCConfig.fullCoverage(), new NYCConfig().fullCoverage());
-	t.deepEqual(NYCConfig.fullCoverage(true), new NYCConfig().fullCoverage(true));
-	t.deepEqual(NYCConfig.fullCoverage(false), new NYCConfig().fullCoverage(false));
-});
-
-test('require', t => {
-	t.is(typeof NYCConfig.require, 'function');
-	t.true(NYCConfig.require() instanceof NYCConfig);
-
-	t.deepEqual(NYCConfig.require().settings.exclude, defaultExclude);
-
-	t.snapshot({
-		...NYCConfig.require().settings,
-		exclude: []
-	}, 'no arg');
-
-	t.snapshot({
-		...NYCConfig.require('esm', '@babel/register').settings,
-		exclude: []
-	}, 'esm and babel');
-
-	t.deepEqual(NYCConfig.require(), new NYCConfig().require());
-	t.deepEqual(NYCConfig.require('esm', '@babel/register'), new NYCConfig().require('esm', '@babel/register'));
-});
-
-test('include', t => {
-	t.is(typeof NYCConfig.include, 'function');
-	t.true(NYCConfig.include() instanceof NYCConfig);
-
-	t.deepEqual(NYCConfig.include().settings.exclude, defaultExclude);
-
-	t.snapshot({
-		...NYCConfig.include().settings,
-		exclude: []
-	}, 'no arg');
-
-	t.snapshot({
-		...NYCConfig.include('file1.js', 'file2.js').settings,
-		exclude: []
-	}, 'esm and babel');
-
-	t.deepEqual(NYCConfig.include(), new NYCConfig().include());
-	t.deepEqual(NYCConfig.include('file1.js', 'file2.js'), new NYCConfig().include('file1.js', 'file2.js'));
-});
-
-test('exclude', t => {
-	t.is(typeof NYCConfig.exclude, 'function');
-	t.true(NYCConfig.exclude() instanceof NYCConfig);
-
-	t.deepEqual(NYCConfig.exclude().settings.exclude, defaultExclude);
-
-	t.snapshot({
-		...NYCConfig.exclude().settings,
-		exclude: []
-	}, 'no arg');
-
-	const {settings} = NYCConfig.exclude('file1.js', 'file2.js');
-	settings.exclude = settings.exclude.filter(e => !defaultExclude.includes(e));
-	t.snapshot(settings, 'file1.js and file2.js');
-
-	t.deepEqual(NYCConfig.exclude(), new NYCConfig().exclude());
-	t.deepEqual(NYCConfig.exclude('file1.js', 'file2.js'), new NYCConfig().exclude('file1.js', 'file2.js'));
-});
-
-test('all', t => {
 	const obj = new NYCConfig();
-
-	t.deepEqual(obj.settings.exclude, defaultExclude);
-
-	t.true(obj.settings.all);
-
-	obj.all();
-	t.false(obj.settings.all);
-
-	obj.all(true);
-	t.true(obj.settings.all);
-
-	obj.all(false);
-	t.false(obj.settings.all);
-
-	obj.all(true);
-	t.deepEqual(obj, new NYCConfig());
+	t.is(obj.settings, obj[Symbol.for('nycrc')]);
 });
 
-test('excludeNodeModules', t => {
-	const obj = new NYCConfig();
+test('all', booleanArgTest);
 
-	t.true(obj.settings.excludeNodeModules);
+test('fullCoverage', booleanArgTest);
 
-	obj.excludeNodeModules();
-	t.false(obj.settings.excludeNodeModules);
-
-	obj.excludeNodeModules(true);
-	t.true(obj.settings.excludeNodeModules);
-
-	obj.excludeNodeModules(false);
-	t.false(obj.settings.excludeNodeModules);
-
-	obj.excludeNodeModules(true);
-	t.deepEqual(obj, new NYCConfig());
+test('require', stringsArgTest, {
+	'esm and babel': ['esm', '@babel/register']
 });
+
+test('include', stringsArgTest, {
+	'file1.js and file2.js': ['file1.js', 'file2.js']
+});
+
+test('exclude', stringsArgTest, {
+	'file1.js and file2.js': ['file1.js', 'file2.js']
+});
+
+test('excludeNodeModules', booleanArgTest);
